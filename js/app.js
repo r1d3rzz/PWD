3e5f3682787e0919072762ebe9227e08166fbbcd
r1d3rzz <myintthwaykhine127@gmail.com>
2023-10-07 16:52:54 +0630

    day 1

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  if (a > b) {
+    return a - b;
+  } else {
+    return b - a;
+  }
+}
+
+// Test In JavaScript
+
+for (let i = 0; i < array.length; i++) {
+  function show() {
+    const element = array[i];
+  }
+}
+
+function hide() {}
+function dialog() {}
+
+var user = { name: "rider", age: 21 };
+
+// Control + G show line number
+// Control + Shift + O show function symbol / name
+
+function Test(Test) {
+  //
+}
